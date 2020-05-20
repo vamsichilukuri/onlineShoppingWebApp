a8eaf7b923897f37ad0cc6829c6fd76a98032a01
Vamsi <58515346+vamsichilukuri@users.noreply.github.com>
2020-05-20 11:33:06 +0530

    initial

---
+++ js/womenMainIteams.js
@@ -0,0 +1,41 @@
+var mainProducts = [
+  {
+    price: 550,
+    image: "./images/women/women1.jpeg",
+  },
+  {
+    price: 950,
+    image: "./images/women/women2.jpeg",
+  },
+  {
+    price: 450,
+    image: "./images/women/women3.jpeg",
+  },
+  {
+    price: 550,
+    image: "./images/women/women4.jpeg",
+  },
+  {
+    price: 950,
+    image: "./images/women/women5.jpeg",
+  },
+];
+var mainIteams = () => {
+  for (let i = 0; i < mainProducts.length; i++) {
+    productsList(mainProducts[i]);
+  }
+};
+function productsList(data) {
+  var ulTag = $("<ul></ul>").addClass("iteams");
+  var li1 = $("<li></li>").text(`Price : $ ${data.price}`);
+  var li2 = $("<li></li>");
+  var li3 = $("<li></li").addClass("add-cart-icon");
+  li3.text("+ Add To");
+  var i = $("<i></i>").addClass("fas fa-cart-plus add-cart-icon");
+  li3.append(i);
+  var imgTag = $("<img>").attr("src", data.image);
+  li2.append(imgTag);
+  ulTag.append(li2, li1, li3);
+  $(".women-product-block").append(ulTag);
+}
+mainIteams();
